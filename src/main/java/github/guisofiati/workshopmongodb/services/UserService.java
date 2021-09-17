bcc25fe804beb73f11d6e7cb46c179bb82e5404f
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-17 09:50:54 -0300

    Find user by id

--- src/main/java/github/guisofiati/workshopmongodb/services/UserService.java
+++ src/main/java/github/guisofiati/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package github.guisofiati.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import github.guisofiati.workshopmongodb.domain.User;
 import github.guisofiati.workshopmongodb.repositories.UserRepository;
+import github.guisofiati.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
