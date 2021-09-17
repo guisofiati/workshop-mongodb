70e0f5663be267d7c381b5520a7add09452ac4a3
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-17 11:59:40 -0300

    Added comments to the posts

--- src/main/java/github/guisofiati/workshopmongodb/config/Instantiation.java
+++ src/main/java/github/guisofiati/workshopmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import github.guisofiati.workshopmongodb.domain.Post;
 import github.guisofiati.workshopmongodb.domain.User;
 import github.guisofiati.workshopmongodb.dto.AuthorDTO;
+import github.guisofiati.workshopmongodb.dto.CommentDTO;
 import github.guisofiati.workshopmongodb.repositories.PostRepository;
 import github.guisofiati.workshopmongodb.repositories.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraçaos!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia.", "Acordei feliz hoje kkk", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(jose));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(joao));
+		CommentDTO c3 = new CommentDTO("Boa viagem mano!", sdf.parse("23/03/2018"), new AuthorDTO(jose));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	
